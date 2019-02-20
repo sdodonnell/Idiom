ab94c967f1b96165fd8ee9f3295d76615c1c4056
Sam <sam.d.odonnell@gmail.com>
2019-02-20 12:22:05 -0800

    Finish migrations

--- db/migrate/20190220170934_create_likes.rb
+++ db/migrate/20190220170934_create_likes.rb
@@ -2,7 +2,7 @@ class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
       t.integer :user_id, null: false
-      t.inetger :story_id, null: false
+      t.integer :story_id, null: false
       t.timestamps
     end
   end
