0ba08fc2f1c38454237a9df4889201080637ba48
Sam <sam.d.odonnell@gmail.com>
2019-02-26 12:30:07 -0800

    Add routes and entry/root/app files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user
+
+    def login!(user)
+        @current_user = user
+        session[:session_token] = @current_user.reset_session_token!
+    end
+
+    def logout
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
