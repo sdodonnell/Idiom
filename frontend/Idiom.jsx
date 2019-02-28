71d0375ee68bc7e4adabb4909b6367e57c468f53
Sam <sam.d.odonnell@gmail.com>
2019-02-27 17:39:26 -0800

    Start exclusion test with frontend

--- frontend/Idiom.jsx
+++ frontend/Idiom.jsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './components/root';
-import { configureStore } from './store/store';
-import { signup } from './actions/session_actions'
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Root from './components/root';
+// import { configureStore } from './store/store';
+// import { signup } from './actions/session_actions'
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    let store;
+// document.addEventListener('DOMContentLoaded', () => {
+//     let store;
     
-    // Boostrapping current user
-    if (window.currentUser) {
-        const preloadedState = {
-          entities: {
-            users: { [window.currentUser.id]: window.currentUser }
-          },
-          session: { id: window.currentUser.id }
-        };
-        store = configureStore(preloadedState);
-        delete window.currentUser;
-      } else {
-        store = configureStore();
-      }
-    //
+//     // Boostrapping current user
+//     if (window.currentUser) {
+//         const preloadedState = {
+//           entities: {
+//             users: { [window.currentUser.id]: window.currentUser }
+//           },
+//           session: { id: window.currentUser.id }
+//         };
+//         store = configureStore(preloadedState);
+//         delete window.currentUser;
+//       } else {
+//         store = configureStore();
+//       }
+//     //
 
-    // TESTING
-    window.getState = store.getState
-    window.dispatch = store.dispatch
-    window.signup = signup
-    // TESTING
+//     // TESTING
+//     window.getState = store.getState
+//     window.dispatch = store.dispatch
+//     window.signup = signup
+//     // TESTING
 
     
-    ReactDOM.render(
-        <Root store={store}/>,
-        document.getElementById('root')
-    )
-})
+//     ReactDOM.render(
+//         <Root store={store}/>,
+//         document.getElementById('root')
+//     )
+// })
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/bananas.jsx',
+  entry: './frontend/idiom.jsx',
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
