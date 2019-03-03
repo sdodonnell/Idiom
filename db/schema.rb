8a35dfab46bfd4ad28437e7973a132de57ebb054
Sam <sam.d.odonnell@gmail.com>
2019-03-03 14:04:46 -0800

    Add user seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_02_202121) do
+ActiveRecord::Schema.define(version: 2019_03_03_201440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2019_03_02_202121) do
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "fullname", null: false
     t.index ["session_token"], name: "index_users_on_session_token"
     t.index ["username"], name: "index_users_on_username"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,104 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+Story.destroy_all
 
-u = User.new(
+
+User.create!(
+    username: "Seneca",
+    fullname: "Seneca"
+    email: "seneca@essaysbyseneca.com",
+    password: "tantastultitia"
+)
+User.create!(
+    username: "Plutarch",
+    fullname: "Plutarch"
+    email: "plutarch@essaysbyplutarch.com",
+    password: "worldcitizen"
+)
+User.create!(
+    username: "DesideriusErasmus",
+    fullname: "Erasmus"
+    email: "erasmus@essaysbyerasmus.com",
+    password: "praiseroffolly"
+)
+User.create!(
     username: "MichelDeMontaigne",
+    fullname: "Michel de Montaigne"
     email: "michel@essaysbymichel.com",
     password: "cannibalsrule"
 )
+User.create!(
+    username: "MarkTwain",
+    fullname: "Mark Twain"
+    email: "mark@essaysbytwain.com",
+    password: "samuelclemens"
+    )
+User.create!(
+    username: "HenryDavidThoreau",
+    fullname: "Henry David Thoreau"
+    email: "henry@essaysbythoreau.com",
+    password: "cannibalsrule"
+)
+User.create!(
+    username: "RalphWaldoEmerson",
+    fullname: "Ralph Waldo Emerson"
+    email: "ralph@essaysbyemerson.com",
+    password: "civildisobedience"
+)
+User.create!(
+    username: "AnnaHowardShaw",
+    fullname: "Anna Howard Shaw"
+    email: "anna@essaysbyshaw.com",
+    password: "votingrights4women"
+)
+User.create!(
+    username: "JohnMuir",
+    fullname: "John Muir"
+    email: "john@essaysbymuir.com",
+    password: "goforawalk"
+)
+User.create!(
+    username: "ZitkalaSa",
+    fullname: "Zitkálá-Šá"
+    email: "zs@essaysbyzs.com",
+    password: "cannibalsrule"
+)
+User.create!(
+    username: "OscarWilde",
+    fullname: "Oscar Wilde"
+    email: "oscar@essaysbywilde.com",
+    password: "mysteryoflove"
+    )
+User.create!(
+    username: "WEBDuBois",
+    fullname: "W.E.B. DuBois"
+    email: "william@essaysbydubois.com",
+    password: "freedomdawns"
+)
+User.create!(
+    username: "MaxBeerbohm",
+    fullname: "Max Beerbohm"
+    email: "max@essaysbybeerbohm.com",
+    password: "tothemax"
+    )
+User.create!(
+    username: "VirginiaWoolf",
+    fullname: "Virginia Woolf"
+    email: "virginia@essaysbywoolf.com",
+    password: "aroomofmyown"
+    )            
+User.create!(
+    username: "MohandasKGandhi",
+    fullname: "Mahātmā Gandhi"
+    email: "mohandas@essaysbygandhi.com",
+    password: "satyagraha"
+)
+User.create!(
+    username: "FrantzFanon",
+    fullname: "Frantz Fanon"
+    email: "frantz@essaysbyfanon.com",
+    password: "peaunoir"
+)
 
-u.save
+Story.create
