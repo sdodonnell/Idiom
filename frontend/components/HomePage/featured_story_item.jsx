9e916795548e4aa5c6394f4571435be0366e8fb7
Sam <sam.d.odonnell@gmail.com>
2019-03-06 08:11:37 -0800

    Add data and styling to Featured component

--- frontend/components/HomePage/featured_story_item.jsx
+++ frontend/components/HomePage/featured_story_item.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const FeaturedStoryItem = () => (
-    <div className="featured-story-item">
-
-    </div>
-)
+const FeaturedStoryItem = props => {
+    if (props.story) {
+        return (
+            <div className="featured-story-item">
+                <div className="homepage-image">
+                    <img src={props.story.photoUrls[0]}/>
+                </div>
+                <h3>{props.story.titlePreview}</h3>
+                <p>{props.story.bodyPreview}</p>
+            </div>
+        )} else {
+            return <div></div>
+        }
+    }
 
 export default FeaturedStoryItem
--- frontend/components/HomePage/main_featured_story_item.jsx
+++ frontend/components/HomePage/main_featured_story_item.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const MainFeaturedStoryItem = () => (
-    <div className="main-featured-story-item">
-    </div>
-)
+const MainFeaturedStoryItem = props => {
+if (props.story) {
+    return (
+        <div className="main-featured-story-item">
+            <div className="homepage-image">
+                <img src={props.story.photoUrls[0]}/>
+            </div>
+            <h2>{props.story.titlePreview}</h2>
+            <p>{props.story.bodyPreview}</p>
+        </div>
+    )} else {
+        return <div></div>
+    }
+}
 
 export default MainFeaturedStoryItem
